c3f8e5a6d0801d8a503def5d5da5201e2d7b2fe9
PrasannaMarihalkar <prasannacm.in@email.com>
2026-07-03 23:57:22 +0530

    Fix flaky cart navigation test with explicit wait

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class InventoryPage:
     ADD_TO_CART_BUTTONS = (By.CSS_SELECTOR, "button.btn_inventory")
     CART_BADGE = (By.CLASS_NAME, "shopping_cart_badge")
@@ -17,6 +18,7 @@ class InventoryPage:
 
     def go_to_cart(self):
         self.driver.find_element(*self.CART_LINK).click()
+        WebDriverWait(self.driver, 10).until(EC.url_contains("cart.html"))
 
     def get_item_count(self):
         return len(self.driver.find_elements(*self.INVENTORY_ITEMS))
